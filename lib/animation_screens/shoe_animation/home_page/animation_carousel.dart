dfaa6fddfdf9ddfb22fb790e3225d91581c5a476
Sahil Garg <sahilg@geekyants.com>
2021-06-10 11:58:55 +0530

    Added rotation to shoe during Hero Animation

--- lib/animation_screens/shoe_animation/home_page/animation_carousel.dart
+++ lib/animation_screens/shoe_animation/home_page/animation_carousel.dart
@@ -37,6 +37,25 @@ class _AnimationCarouselState extends State<AnimationCarousel>
 
   @override
   Widget build(BuildContext context) {
+    Widget heroFlightshuttleBuilder(
+      BuildContext flightContext,
+      Animation<double> animation,
+      HeroFlightDirection direction,
+      BuildContext fromContext,
+      BuildContext toContext,
+      int index,
+    ) {
+      // HeroFlightDirection.pop
+      print('Hero animation status:${animation.status}');
+      final Hero toHero = toContext.widget;
+      return RotationTransition(
+        turns: animation.drive(
+          Tween(begin: -0.1, end: 0),
+        ),
+        child: toHero.child,
+      );
+    }
+
     return Container(
       // color: Colors.blueAccent,
       color: Colors.transparent,
@@ -59,101 +78,142 @@ class _AnimationCarouselState extends State<AnimationCarousel>
             ),
             // color: Colors.grey,
             color: Colors.transparent,
-            child: GestureDetector(
-              onTap: () {
-                print('Going to detial screen with index : $index');
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => ShoeDescription(index: index)));
-              },
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: Container(
-                      margin: EdgeInsets.only(
-                        right: scaleConfig.scaleHeight(40),
-                        top: scaleConfig.scaleHeight(35),
-                        bottom: scaleConfig.scaleHeight(20),
-                      ),
-                      padding:
-                          EdgeInsets.only(left: scaleConfig.scaleHeight(21)),
-                      decoration: BoxDecoration(
-                        color: ShoeAssets
-                            .colorList[index >= 5 ? index % 5 : index],
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 30),
-                          Text(
-                            ShoeAssets.names[index >= 5 ? index % 5 : index],
-                            style: TextStyle(
-                              color: index % 2 == 0
-                                  ? Colors.white
-                                  : Colors.black87,
-                              fontSize: 23,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            ShoeAssets.prices[index >= 5 ? index % 5 : index],
-                            style: TextStyle(
-                              color: index % 2 == 0
-                                  ? Colors.white70
-                                  : Colors.black45,
-                              fontSize: 19,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Padding(
-                            padding: EdgeInsets.only(left: 4),
-                            child: Container(
-                              width: 1.5,
-                              height: 150,
-                              color: index % 2 == 0
-                                  ? Colors.white30
-                                  : Colors.black12,
-                            ),
-                          ),
-                        ],
-                      ),
+            child: Stack(
+              children: [
+                //Card
+                Positioned.fill(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      right: scaleConfig.scaleHeight(40),
+                      top: scaleConfig.scaleHeight(35),
+                      bottom: scaleConfig.scaleHeight(20),
                     ),
-                  ),
-                  Positioned(
-                    bottom: 30,
-                    right: -25,
-                    child: AnimatedBuilder(
-                      animation: _animationController,
-                      builder: (context, widget) {
-                        return Transform.rotate(
-                          angle: (index == _currentPageIndex)
-                              ? _animationController.isAnimating
-                                  ? rotationAnimation.value
-                                  : -degree * 30
-                              : _animationController.isAnimating
-                                  ? (rotationAnimation.value + degree * 30)
-                                  : -degree * 60,
+                    padding: EdgeInsets.only(left: scaleConfig.scaleHeight(21)),
+                    decoration: BoxDecoration(
+                      color:
+                          ShoeAssets.colorList[index >= 5 ? index % 5 : index],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 30),
+                        Text(
+                          ShoeAssets.names[index >= 5 ? index % 5 : index],
+                          style: TextStyle(
+                            color:
+                                index % 2 == 0 ? Colors.white : Colors.black87,
+                            fontSize: 23,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          ShoeAssets.prices[index >= 5 ? index % 5 : index],
+                          style: TextStyle(
+                            color: index % 2 == 0
+                                ? Colors.white70
+                                : Colors.black45,
+                            fontSize: 19,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Padding(
+                          padding: EdgeInsets.only(left: 4),
                           child: Container(
-                            color: Colors.transparent,
-                            height: 184,
-                            width: 320,
-                            child: Hero(
-                              tag: 'shoe' + index.toString(),
-                              child: Image.asset(
-                                ShoeAssets
-                                    .images[index >= 3 ? index % 3 : index],
-                                fit: BoxFit.fitWidth,
-                              ),
-                            ),
+                            width: 1.5,
+                            height: 150,
+                            color: index % 2 == 0
+                                ? Colors.white30
+                                : Colors.black12,
                           ),
-                        );
-                      },
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                //shoe
+                Positioned(
+                  bottom: 30,
+                  right: -25,
+                  child: AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, widget) {
+                      return Transform.rotate(
+                        angle: (index == _currentPageIndex)
+                            ? _animationController.isAnimating
+                                ? rotationAnimation.value
+                                : -degree * 30
+                            : _animationController.isAnimating
+                                ? (rotationAnimation.value + degree * 30)
+                                : -degree * 60,
+                        child: Container(
+                          color: Colors.transparent,
+                          height: 184,
+                          width: 320,
+                          child: Hero(
+                            tag: 'shoe' + index.toString(),
+                            transitionOnUserGestures: true,
+                            // createRectTween: (begin, end) {
+                            //   return RectTween(
+                            //     // begin: Rect.fromCenter(
+                            //     //   center: begin.center,
+                            //     //   width: begin.width,
+                            //     //   height: begin.height,
+                            //     // ),
+                            //     // end: Rect.fromPoints(
+                            //     //   // Offset(300, 700),
+                            //     //   // Offset(400, 300),
+                            //     //   Offset(500, 500),
+                            //     //   Offset(0, 0),
+                            //     // ),
+                            //     begin: Rect.fromCircle(
+                            //         center: begin.center, radius: radius),
+                            //     end: Rect.fromCenter(
+                            //       center: end.center,
+                            //       width: end.width,
+                            //       height: end.height,
+                            //     ),
+                            //   );
+                            // },
+                            flightShuttleBuilder: (flightContext, animation,
+                                direction, fromContext, toContext) {
+                              return heroFlightshuttleBuilder(
+                                flightContext,
+                                animation,
+                                direction,
+                                fromContext,
+                                toContext,
+                                index,
+                              );
+                            },
+                            child: Builder(
+                              builder: (context) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    print(
+                                        'Going to detial screen with index : $index');
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ShoeDescription(index: index)));
+                                  },
+                                  child: Image.asset(
+                                    ShoeAssets
+                                        .images[index >= 3 ? index % 3 : index],
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           );
         },
