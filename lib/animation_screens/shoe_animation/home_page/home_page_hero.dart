2ee49ca121199cc761fc3abd6e8fe10baa6fa5d1
Sahil Garg <sahilg@geekyants.com>
2021-06-10 16:58:45 +0530

    Achieved the smooth Hero Animation to & from home page

--- lib/animation_screens/shoe_animation/home_page/home_page_hero.dart
+++ lib/animation_screens/shoe_animation/home_page/home_page_hero.dart
@@ -10,47 +10,29 @@ class HomePageHero extends StatelessWidget {
       tag: tag,
       transitionOnUserGestures: true,
       createRectTween: (begin, end) {
-        // print('end top right- dx:${end.topRight.dx}, dy:${end.topRight.dy}');
-        // print(
-        //     'end bottom left- dx:${end.bottomLeft.dx}, dy:${end.bottomLeft.dy}');
         return RectTween(
-          // Rect.fromLTWH(left, top, width, height)
-          // begin: Rect.fromCenter(
-          //   center: begin.center,
-          //   width: begin.width,
-          //   height: begin.height,
-          // ),
           begin: Rect.fromPoints(
-            begin.topRight, begin.bottomLeft,
-            // Offset(begin.topRight.dx,
-            //     begin.topRight.dy - 35),
-            // Offset(begin.bottomLeft.dx,
-            //     begin.bottomLeft.dy - 35),
+            begin.topRight,
+            begin.bottomLeft,
           ),
           end: Rect.fromPoints(
-            Offset(end.topRight.dx, end.topRight.dy - 135),
-            Offset(end.bottomLeft.dx, end.bottomLeft.dy - 135),
+            Offset(end.topRight.dx + 25, end.topRight.dy - 160),
+            Offset(end.bottomLeft.dx + 25, end.bottomLeft.dy - 160),
           ),
-          // end: Rect.fromCircle(
-          //     center: end.center, radius: end.height / 2),
         );
       },
       flightShuttleBuilder:
           (flightContext, animation, direction, fromContext, toContext) {
         print('Hero animation status:${animation.status}');
         final Hero toHero = toContext.widget;
-        // final Hero fromHero = fromContext.widget;
         return SizeTransition(
-          sizeFactor: animation.drive(Tween<double>(begin: 1, end: 2.5)),
+          sizeFactor: animation, // in order to avoid size variation
           child: RotationTransition(
-              turns: animation.drive(
-                Tween<double>(begin: -0.1, end: 0),
-              ),
-              child:
-                  // direction == HeroFlightDirection.push ?
-                  toHero.child
-              // : fromHero.child,
-              ),
+            turns: animation.drive(
+              Tween<double>(begin: -30 / 360, end: 0),
+            ),
+            child: toHero.child,
+          ),
         );
       },
       child: child,
